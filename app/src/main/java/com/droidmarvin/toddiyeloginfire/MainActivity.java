e08d35e179752d8e1a8cace8e5428f2cf8b9f1dc
ngesa marvin <marvinngesa@gmail.com>
2018-01-18 10:26:38 +0300

    create the welcome screen after login

--- app/src/main/java/com/droidmarvin/toddiyeloginfire/MainActivity.java
+++ app/src/main/java/com/droidmarvin/toddiyeloginfire/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import com.droidmarvin.toddiyeloginfire.Model.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
